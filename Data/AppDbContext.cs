bfb575b8bb421a175714085ecab1b19e4f3ff0a6
zonhanoz09 <huynhvannhancntt@gmail.com>
2021-09-12 23:14:52 +0700

    add

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+using CommanderGQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuider)
+        {
+            modelBuider
+                .Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(p => p.Platform)
+                .HasForeignKey(p => p.PlatformId);
+
+            modelBuider
+                .Entity<Command>()
+                .HasOne(p => p.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(p => p.PlatformId);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using GraphQL.Server.Ui.Voyager;
+using CommanderGQL.GraphQL.Platforms;
+using CommanderGQL.GraphQL.Commands;
+
+namespace CommanderGQL
+{
+    public class Startup
+    {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddPooledDbContextFactory<AppDbContext>(opt => opt.UseSqlServer(
+                Configuration.GetConnectionString("CommandConStr")));
+
+            services.AddGraphQLServer()
+                    .AddQueryType<Query>()
+                    .AddMutationType<Mutation>()
+                    .AddSubscriptionType<Subscription>()
+                    .AddType<PlatformType>()
+                    .AddType<CommandType>()
+                    .AddFiltering()
+                    .AddSorting()
+                    .AddInMemorySubscriptions();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+
+            app.UseGraphQLVoyager(new GraphQLVoyagerOptions()
+            {
+                GraphQLEndPoint = "/graphql",
+                Path = "/graphql-voyager"
+            });
+        }
+    }
+}
